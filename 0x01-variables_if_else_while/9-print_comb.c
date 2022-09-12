e276f6a8a5b1dd675e59e500995f88fdcf5a0fd5
EvalynMw <evalyn.maina@gmail.com>
2022-09-12 14:35:52 -0700

    9 print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print if the numbers is positive, zero, or negative
+ * main - printing numbers from 0-9 with commas and space between them
  * Description: using the main function
  * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
  * Return: 0
@@ -20,4 +20,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}	
+}
